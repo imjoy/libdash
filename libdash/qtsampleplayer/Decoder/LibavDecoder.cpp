cf9235493076262021023ec3e690a7d948e7ce13
imjoy <callbyflower@hotmail.com>
2018-03-23 16:57:20 +0900

    modify for mac

--- libdash/qtsampleplayer/Decoder/LibavDecoder.cpp
+++ libdash/qtsampleplayer/Decoder/LibavDecoder.cpp
@@ -70,9 +70,9 @@ void                LibavDecoder::NotifyVideo             (AVFrame * avFrame, St
     props.linesize      = avFrame->linesize;
     props.streamIndex   = decoConf->stream->index;
 
-    if(decoConf->stream->codec->pix_fmt == PIX_FMT_YUV420P)
+    if(decoConf->stream->codec->pix_fmt == AV_PIX_FMT_YUV420P)
         props.pxlFmt = yuv420p;
-    if(decoConf->stream->codec->pix_fmt == PIX_FMT_YUV422P)
+    if(decoConf->stream->codec->pix_fmt == AV_PIX_FMT_YUV422P)
         props.pxlFmt = yuv422p;
 
     for(size_t i = 0; i < videoObservers.size(); i++)
@@ -254,7 +254,11 @@ bool                LibavDecoder::Init                    ()
     if (this->errorHappened)
         return false;
 
+#if LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(55, 45, 101)
+      this->frame = av_frame_alloc();
+#else
     this->frame = avcodec_alloc_frame();
+#endif
 
     av_init_packet(&this->avpkt);
     this->InitStreams(avFormatContextPtr);
